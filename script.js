5ac9b1ad1a5e2bced79b13dbcbb7538d9158dcd1
pedrogamedev <pedrobrunigavioli@gmail.com>
2023-11-13 15:35:45 -0300

    Progresso no visual e no js

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
 
-/*  Codex:
+/*  
+    Neste projeto eu usei um conceito modular, onde somente o createGameMaster
+    cuida do game, com excecao do botao que eh necessario para comecar o game
+    
+    Codex:
 
     GameMaster: responsavel por criar os objetos do jogo e
     cuida do estado do jogo
     
     GameTable: Cuida da posicao dos objetos no jogo da velha
     e a sua checagem
+
+    StartBtn: botao usado para comecar o game
+
+    Player control: cuida das vitorias dos players, o visual,
+    e o texto de player wins
 */
 
 /*--- Factory Functions ---*/
@@ -27,7 +36,7 @@ function createStartBtn()
         game.startGame();
     })
 
-    return { enableStartBtn}
+    return {enableStartBtn}
 }
 
 
@@ -40,26 +49,32 @@ function createGameMaster()
     let table = createGameTable();
     let playerControl = createPlayerControl();
 
-    // Setando variaveis
-    let curPlayer = 1;
-
     // Comando para mudar o player a cada Jogada
-    const changePlayer = () =>
-    {
-        curPlayer == 1 ? 2 : 1;
-    }
     
     const startRound = () =>
     {
         table.restartTable();
     }
+    const updateRound = () =>
+    {
+        playerControl.changePlayer();
+        playerControl.updateText();
+
+        let player = table.checkCells();
+        
+        if(player != null)
+        {
+            endRound(player);
+        }
+    }
     const endRound = (player) =>
     {
-        let temp = playerControl.updateWins(player);
+        playerWon = playerControl.updateWins(player);
 
-        if(temp)
+        table.restartTable();
+        if(playerWon)
         {
-            endGame(player);
+            endGame(player)
         }
     }
 
@@ -71,25 +86,35 @@ function createGameMaster()
     const endGame = (player) =>
     {
         alert("Congratulations player "+player+" for winning!");
-        playerControl.resetWins();
+        playerControl.resetStats();
         startBtn.enableStartBtn();
     }
-    return {startRound, endRound, startGame, endGame, changePlayer};
+    return {startRound, updateRound, startGame, endGame, playerControl};
 }
 
+// esse daq ta modular
 function createPlayerControl()
 {
     //player 1 and 2 number of wins
 
-
-
+    let currentPlayer = 1;
     let oneWins = 0;
     let twoWins = 0;
 
     const playerText = document.getElementById('currentPlayer');
 
-    const resetWins = () =>
+    const changePlayer = () =>
     {
+        currentPlayer = currentPlayer == 1 ? 2 : 1;
+    }
+
+    const CurrentPlayer = () =>
+    {
+        return currentPlayer;
+    }
+    const resetStats = () =>
+    {
+        currentPlayer = 1;
         oneWins = 0;
         twoWins = 0;
     }
@@ -107,18 +132,15 @@ function createPlayerControl()
         }
     }
 
-    const updateText = (player) =>
+    const updateText = () =>
     {
-        
+        playerText.innerHTML = "Player " + currentPlayer;
     }
     
-
-
-    return { oneWins, twoWins, resetWins, updateWins}
+    return {oneWins, twoWins, resetStats, updateWins,updateText, changePlayer, CurrentPlayer}
 }
 
 //GameTable
-
 
 function createGameTable()
 {
@@ -135,7 +157,7 @@ function createGameTable()
         {
             btn = document.getElementById('cellBtn'+curBtn);
             btn.disabled = true;
-            table[i][j] = [createCellBtn(btn)];
+            table[i][j] = createCellBtn(btn);
             curBtn++;
         }
     }
@@ -158,37 +180,37 @@ function createGameTable()
         {
             btn = document.getElementById('cellBtn' + curBtn);
             btn.activated = false;
-            curBtn;
+            curBtn++;
         }
     }
 
+
+    // refazer esse codigo
     const checkCells = () =>
     {
         for(i = 0;i<3; i++)
         {
-            if(table[i][0].player == table[i][1].player == table[i][2])
+            if(table[i][0] != 0 && table[i][0].CurrentPlayer() == table[i][1].CurrentPlayer() == table[i][2].CurrentPlayer())
             {
-                game.endRound(table[i][0].player);
-                return;
+                return table[i][0].CurrentPlayer();
             }
         }
         for(i = 0;i<3; i++)
         {
-            if(table[0][j].player == table[1][j].player == table[2][j])
+            if(table[0][i] != 0 && table[0][i].CurrentPlayer() == table[1][i].CurrentPlayer() == table[2][i].CurrentPlayer())
             {
-                game.endRound(table[0][i].player);
-                return;
+                return table[0][i].CurrentPlayer();
             }
         }
-        if(table[0][0].player == table[1][1].player == table[2][2].player ||
-        table[0][2].player == table[1][1].player == table[2][0].player)
+        if(table[i][i] != 0 && (table[0][0].CurrentPlayer() == table[1][1].CurrentPlayer() == table[2][2].CurrentPlayer() ||
+        table[0][2].CurrentPlayer() == table[1][1].CurrentPlayer() == table[2][0].CurrentPlayer()))
         {
-            game.endRound(table[1][1].player);
-            return;
+            return table[1][1].CurrentPlayer();
         }
+        return null;
     }
 
-    return { startTable, restartTable, checkCells}
+    return {startTable, restartTable, checkCells}
 }
 
 
@@ -199,22 +221,24 @@ function createCellBtn (dom)
 {
     const domElement = dom;
     let activated = false;
-    player = 0;
+    let player = 0;
 
 
     domElement.addEventListener('click', ()=>
     {
         if(!activated)
         {
-            alert("hello i work");
+            player = game.playerControl.CurrentPlayer();
+            domElement.classList.add("activated", "markedBtn"+player);
+            game.updateRound();
             activated = true;
-            player = game.curPlayer;
-            domElement.classList.add("markedBtn");
-            game.changePlayer();
         }
     })
-}
 
+    const CurrentPlayer = () => { return player}
+
+    return {CurrentPlayer}
+}
 
 //Criando objetos 
 let game = createGameMaster();
